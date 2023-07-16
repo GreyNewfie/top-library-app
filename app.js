bed833bf3663252b5426f5bd655ce63e3bebd0ca
Danny Simms <djfsimms@hotmail.com>
2023-07-16 11:13:18 -0230

    Add book cards and read book select field
    Added cards to represent books that the user is tracking. Add html
    cards to arrange layout and basic design.
    Changed the "Have you read this book?" form field to a select field
    for look and UX.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const submitBook = document.getElementById("submit-book");
+const bookTitleField = document.getElementById("book-title");
+const bookAuthorField = document.getElementById("book-author");
+const bookLengthField = document.getElementById("book-pages");
+const formElem = document.querySelector("form");
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,3 +14,19 @@ function Book(title, author, pages, read) {
 Book.prototype.info = function () {
     return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
 }
+
+let newBookTitle = "";
+let newBookAuthor = "";
+let newBookLength = 0;
+
+submitBook.addEventListener("click", (e) => {
+    e.preventDefault();
+    newBookTitle = bookTitleField.value;
+    newBookAuthor = bookAuthorField.value;
+    newBookLength = bookLengthField.value;
+
+    const newBook = new Book(newBookTitle, newBookAuthor, newBookLength);
+    console.log(newBook);
+    newBook.info();
+});
+
